e605fe86e00e3a68837f09f02ce5100c11a45fdc
d.akbari <daak1365@yahoo.com>
2019-05-18 13:44:06 +0430

    اضافه کردن انوتیشن ResponsesHttpStatus به منظور اضافه کردن کد وضعیت خطا برای پاسخ به

---
+++ src/main/java/ir/daak/annotation/ResponsesHttpStatus.java
@@ -0,0 +1,13 @@
+package ir.daak.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.http.HttpStatus;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface ResponsesHttpStatus {
+    public HttpStatus value() default HttpStatus.INTERNAL_SERVER_ERROR;
+}
