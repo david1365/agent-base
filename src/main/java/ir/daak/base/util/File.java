f1259e8b7945aaa804876b0d4d51cea426cfec2c
d.akbari <daak1365@yahoo.com>
2019-05-23 20:02:27 +0430

    اضافه کردن وابستگی های ach

--- src/main/java/ir/daak/base/util/File.java
+++ src/main/java/ir/daak/base/util/File.java
@@ -6,13 +6,8 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 
 public class File {
-    public static String readFromFile(String path) {
-        try {
-            byte[] encoded = Files.readAllBytes(Paths.get(path));
-            return new String(encoded, Charset.defaultCharset());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return null;
+    public static String readFromFile(String path) throws IOException {
+        byte[] encoded = Files.readAllBytes(Paths.get(path));
+        return new String(encoded, Charset.defaultCharset());
     }
 }
