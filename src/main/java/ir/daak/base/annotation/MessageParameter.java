ec2c06e547e65fd770a8870377445f52bf78ca47
d.akbari <daak1365@yahoo.com>
2019-05-28 17:57:46 +0430

    تکمیل مدیریت خطاها با گرفتن پارامتر ورودی و چگونگی گرفتن فایل از ورودی

--- src/main/java/ir/daak/base/annotation/MessageParameter.java
+++ src/main/java/ir/daak/base/annotation/MessageParameter.java
@@ -4,10 +4,9 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
-import org.springframework.http.HttpStatus;
 
 @Retention(RetentionPolicy.RUNTIME)
-@Target(ElementType.TYPE)
-public @interface ResponsesHttpStatus {
-    HttpStatus value() default HttpStatus.INTERNAL_SERVER_ERROR;
+@Target(ElementType.METHOD)
+public @interface MessageParameter {
+    int preiority();
 }
